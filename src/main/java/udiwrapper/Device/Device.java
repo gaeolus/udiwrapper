14a8b04c954f1a1cf3bf25c09e8815edc3a18e7f
Ben Smith <bensmith4188@gmail.com>
2016-09-06 12:49:48 -0500

    add Device Classes

---
+++ src/main/java/udiwrapper/Device/Device.java
@@ -0,0 +1,10 @@
+package udiwrapper.Device;
+
+import org.json.JSONObject;
+
+public class Device {
+
+    public Device(JSONObject deviceJson){
+
+    }
+}
---
+++ src/main/java/udiwrapper/Device/UDIDevice.java
@@ -0,0 +1,11 @@
+package udiwrapper.Device;
+
+import org.json.JSONObject;
+
+public class UDIDevice extends Device {
+
+    public UDIDevice(JSONObject deviceJson){
+        super(deviceJson);
+    }
+
+}
