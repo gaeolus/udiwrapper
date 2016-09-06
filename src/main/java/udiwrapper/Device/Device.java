814e8e76f65259b70a5dd2e175d99e79f07cd30e
Ben Smith <bensmith4188@gmail.com>
2016-09-06 13:41:07 -0500

    add properties to Devices

--- src/main/java/udiwrapper/Device/Device.java
+++ src/main/java/udiwrapper/Device/Device.java
@@ -2,8 +2,48 @@ package udiwrapper.Device;
 
 import org.json.JSONObject;
 
-public class Device {
+import java.util.ArrayList;
+import java.util.Calendar;
 
+public class Device {
+    // deliberately omitting GMDNTerm field because of their copyright statement
+    // https://www.gmdnagency.org/Info.aspx?pageid=22
+
+    private boolean DMExempt;
+    private boolean premarketExempt;
+    private boolean deviceHCTP;
+    private boolean deviceKit;
+    private boolean deviceCombinationProduct;
+    private boolean singleUse;
+    private boolean lotBatch;
+    private boolean hasSerialNumber;
+    private boolean hasManufacturingDate;
+    private boolean hasExpirationDate;
+    private boolean hasDonationIdNumber;
+    private boolean labeledContainsNRL;
+    private boolean labeledNoNRL;
+    private boolean rx;
+    private boolean otc;
+    private int deviceCount;
+    private String di;
+    private String recordStatus;
+    private String deviceDescription;
+    private String MRISafetyStatus;
+    private String commercialDistributionStatus;
+    private String brandName;
+    private String versionModelNumber;
+    private String catalogNumber;
+    private String companyName;
+    private Calendar publishDate;
+    private Calendar commercialDistributionEndDate;
+    private ArrayList<Contact> contacts = new ArrayList<>();
+    private ArrayList<ProductCode> productCodes = new ArrayList<>();
+    private ArrayList<DeviceSize> deviceSizes = new ArrayList<>();
+    private ArrayList<EnvironmentalCondition> environmentalConditions = new ArrayList<>();
+    private Sterilization sterilization;
+    private Identifier identifiers;
+
+    // All of the information about a Device is in the JSON response
     public Device(JSONObject deviceJson){
 
     }
--- src/main/java/udiwrapper/Device/UDIDevice.java
+++ src/main/java/udiwrapper/Device/UDIDevice.java
@@ -2,9 +2,22 @@ package udiwrapper.Device;
 
 import org.json.JSONObject;
 
-public class UDIDevice extends Device {
+import java.util.Calendar;
 
-    public UDIDevice(JSONObject deviceJson){
+import okhttp3.Headers;
+
+public class UDIDevice extends Device {
+    private String udi;
+    private String donationId;
+    private String issuingAgency;
+    private String serialNumber;
+    private String lotNumber;
+    private Calendar manufacturingDate;
+    private Calendar expirationDate;
+
+    // The JSON response for a UDI request returns the same JSON as a DI request
+    // but also returns UDI-specific information in the headers of the response
+    public UDIDevice(JSONObject deviceJson, Headers headers){
         super(deviceJson);
     }
 
