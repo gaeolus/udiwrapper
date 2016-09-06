201db31c0a7d54b20ae4848c08a7ee891259ace0
Ben Smith <bensmith4188@gmail.com>
2016-09-06 14:53:56 -0500

    update tests to construct UDIDevice properly

--- src/test/java/udiwrapper/UnitTest.java
+++ src/test/java/udiwrapper/UnitTest.java
@@ -3,6 +3,7 @@ package udiwrapper;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import okhttp3.Headers;
 import udiwrapper.Device.Device;
 import udiwrapper.Device.UDIDevice;
 
@@ -19,6 +20,17 @@ public class UnitTest {
     // JSON string response for DI Device
     private String goodDIDeviceJson = "{\"gudid\":{\"device\":{\"deviceRecordStatus\":\"Published\",\"devicePublishDate\":\"2015-05-08\",\"deviceCommDistributionEndDate\":null,\"deviceCommDistributionStatus\":\"In Commercial Distribution\",\"identifiers\":{\"identifier\":{\"deviceId\":\"08717648200274\",\"deviceIdType\":\"Primary\",\"deviceIdIssuingAgency\":\"GS1\",\"containsDINumber\":null,\"pkgQuantity\":null,\"pkgDiscontinueDate\":null,\"pkgStatus\":null}},\"brandName\":\"XIENCE ALPINE\",\"versionModelNumber\":\"1145350-28\",\"catalogNumber\":\"1145350-28\",\"companyName\":\"ABBOTT VASCULAR INC.\",\"deviceCount\":\"1\",\"deviceDescription\":\"XIENCE Alpine Everolimus Eluting Coronary Stent System 3.50 mm x 28 mm / Over-The-Wire\",\"DMExempt\":\"false\",\"premarketExempt\":\"false\",\"deviceHCTP\":\"false\",\"deviceKit\":\"false\",\"deviceCombinationProduct\":\"true\",\"singleUse\":\"true\",\"lotBatch\":\"true\",\"serialNumber\":\"false\",\"manufacturingDate\":\"false\",\"expirationDate\":\"true\",\"donationIdNumber\":\"false\",\"labeledContainsNRL\":\"false\",\"labeledNoNRL\":\"false\",\"MRISafetyStatus\":\"MR Conditional\",\"rx\":\"true\",\"otc\":\"false\",\"contacts\":{\"customerContact\":{\"phone\":\"+1(800)227-9902\",\"phoneExtension\":null,\"email\":\"AV.CUSTOMERCARE@AV.ABBOTT.COM\"}},\"gmdnTerms\":{\"gmdn\":{\"gmdnPTName\":\"Drug-eluting coronary artery stent, non-bioabsorbable-polymer-coated\",\"gmdnPTDefinition\":\"A sterile non-bioabsorbable metal tubular mesh structure covered with a non-bioabsorbable polymer and a drug coating that is designed to be implanted, via a delivery catheter, into a coronary artery (or saphenous vein graft) to maintain its patency typically in a patient with symptomatic atherosclerotic heart disease. The drug coating is slowly released and intended to inhibit restenosis by reducing vessel smooth muscle cell proliferation. Disposable devices associated with implantation may be included.\"}},\"productCodes\":{\"fdaProductCode\":{\"productCode\":\"NIQ\",\"productCodeName\":\"Coronary drug-eluting stent\"}},\"deviceSizes\":null,\"environmentalConditions\":{\"storageHandling\":[{\"storageHandlingType\":\"Handling Environment Temperature\",\"storageHandlingHigh\":{\"unit\":\"Degrees Celsius\",\"value\":\"30\"},\"storageHandlingLow\":{\"unit\":\"Degrees Celsius\",\"value\":\"15\"},\"storageHandlingSpecialConditionText\":null},{\"storageHandlingType\":\"Special Storage Condition, Specify\",\"storageHandlingHigh\":{\"unit\":\"\",\"value\":\"\"},\"storageHandlingLow\":{\"unit\":\"\",\"value\":\"\"},\"storageHandlingSpecialConditionText\":\"Store in a dry, dark, cool place. Protect from light. Store at 25 degrees C; excursions between 15 to 30 degrees C permitted.\"},{\"storageHandlingType\":\"Storage Environment Temperature\",\"storageHandlingHigh\":{\"unit\":\"Degrees Celsius\",\"value\":\"25\"},\"storageHandlingLow\":{\"unit\":\"Degrees Celsius\",\"value\":\"25\"},\"storageHandlingSpecialConditionText\":null}]},\"sterilization\":{\"deviceSterile\":\"true\",\"sterilizationPriorToUse\":\"false\",\"methodTypes\":null}}}}";
     private String badDIDeviceJson = "{\"error\":\"No device found for DI 0877648200274\"}";
+
+    // Headers returned for UDI
+    Headers headers = new Headers.Builder()
+            .add("donation_id", "A999713123456")
+            .add("di", "08717648200274")
+            .add("lot_number", "000000000000XYZ123")
+            .add("manufacturing_date", "2013-02-01")
+            .add("serial_number", "000025")
+            .add("issuing_agency", "ICCBBA")
+            .add("udi", goodUDI)
+            .build();
 
     // known values for the DI/UDI Device associated with goodDI and goodUDI
     private String brandName = "XIENCE ALPINE";
@@ -65,14 +77,14 @@ public class UnitTest {
     @Test
     public void testGoodUDIDevice() throws Exception {
         JSONObject deviceJson = new JSONObject(goodDIDeviceJson);
-        UDIDevice device = new UDIDevice(deviceJson);
+        UDIDevice device = new UDIDevice(deviceJson, headers);
         assertNull(device);
     }
 
     @Test
     public void testBadUDIDevice() throws Exception {
         JSONObject deviceJson = new JSONObject(badDIDeviceJson);
-        UDIDevice device = new UDIDevice(deviceJson);
+        UDIDevice device = new UDIDevice(deviceJson, headers);
         assertNull(device);
 
     }
