94974cbea43bcd258406d9b6d94dddca1ddb1f45
Ben Smith <bensmith4188@gmail.com>
2016-09-06 12:48:57 -0500

    add methods to UDIWrapper

--- src/main/java/udiwrapper/UDIWrapper.java
+++ src/main/java/udiwrapper/UDIWrapper.java
@@ -1,4 +1,25 @@
 package udiwrapper;
 
+import udiwrapper.Device.Device;
+import udiwrapper.Device.UDIDevice;
+
 public class UDIWrapper {
+
+    // to get whether or not the device exists, either a DI or a UDI can be used
+    public static boolean deviceExists(String deviceID){
+        return false;
+    }
+
+    // fetch the Device information with a DI. Returns the Device if the DI is valid
+    // and null if the DI doesn't exist
+    public static Device fetchDIDevice(String DI){
+        return null;
+    }
+
+    // fetch the Device information with a DI. Returns the Device if the DI is valid
+    // and null if the DI doesn't exist
+    public static UDIDevice fetchUDIDevice(String UDI){
+        return null;
+    }
+
 }
