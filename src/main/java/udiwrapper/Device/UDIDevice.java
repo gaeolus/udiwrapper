bd78bc0e1922eafc4a91e018d5ce27ee90d4083f
Ben Smith <bensmith4188@gmail.com>
2016-09-06 15:12:14 -0500

    add getters for UDI Device

--- src/main/java/udiwrapper/Device/UDIDevice.java
+++ src/main/java/udiwrapper/Device/UDIDevice.java
@@ -38,4 +38,43 @@ public class UDIDevice extends Device {
 
     }
 
+    public String getUdi() {
+        // the Unique Device Identifier for the device
+        return udi;
+    }
+
+    public String getDi() {
+        // the Device Identifier for the device
+        return di;
+    }
+
+    public String getIssuingAgency() {
+        // the issuing agency
+        return issuingAgency;
+    }
+
+    public String getDonationId() {
+        // the donation ID for the device
+        return donationId;
+    }
+
+    public String getSerialNumber() {
+        // the serial number for the device
+        return serialNumber;
+    }
+
+    public String getLotNumber() {
+        // the lot number for the device
+        return lotNumber;
+    }
+
+    public Calendar getManufacturingDate() {
+        // the date the device was manufactured
+        return manufacturingDate;
+    }
+
+    public Calendar getExpirationDate() {
+        // the date the device expires
+        return expirationDate;
+    }
 }
