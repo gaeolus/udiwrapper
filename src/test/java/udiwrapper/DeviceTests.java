37e937e1fd3548062a4fd5b64fc3322b878a4b1e
Ben Smith <bensmith4188@gmail.com>
2016-09-07 11:17:04 -0500

    increase test coverage

--- src/test/java/udiwrapper/DeviceTests.java
+++ src/test/java/udiwrapper/DeviceTests.java
@@ -2,12 +2,17 @@ package udiwrapper;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import udiwrapper.Device.Device;
+import udiwrapper.Device.UDIDevice;
 
 import static org.junit.Assert.*;
 
 public class DeviceTests {
     private static final Device device = UDIWrapper.fetchDIDevice("08717648200274");
+    private static final UDIDevice udiDevice = UDIWrapper.fetchUDIDevice("=/08717648200274=,000025=A99971312345600=>014032=}013032&,1000000000000XYZ123");
 
     @Test
     public void testContact() throws Exception {
@@ -89,4 +94,61 @@ public class DeviceTests {
         assertEquals(true, device.getSterilization().isDeviceSterile());
         assertEquals(false, device.getSterilization().isSterilizePriorToUse());
     }
+
+    @Test
+    public void testStrings() throws Exception {
+        assertEquals("Published", device.getRecordStatus());
+        assertEquals("XIENCE Alpine Everolimus Eluting Coronary Stent System 3.50 mm x 28 mm / Over-The-Wire",
+                device.getDeviceDescription());
+        assertEquals("MR Conditional", device.getMRISafetyStatus());
+        assertEquals("In Commercial Distribution", device.getCommercialDistributionStatus());
+        assertEquals("XIENCE ALPINE", device.getBrandName());
+        assertEquals("1145350-28", device.getVersionModelNumber());
+        assertEquals("1145350-28", device.getCatalogNumber());
+        assertEquals("ABBOTT VASCULAR INC.", device.getCompanyName());
+    }
+
+    @Test
+    public void testBooleans() throws Exception {
+        assertFalse(device.isDMExempt());
+        assertFalse(device.isPremarketExempt());
+        assertFalse(device.isDeviceHCTP());
+        assertFalse(device.isDeviceKit());
+        assertTrue(device.isDeviceCombinationProduct());
+        assertTrue(device.isSingleUse());
+        assertTrue(device.hasLotBatch());
+        assertFalse(device.hasSerialNumber());
+        assertFalse(device.hasManufacturingDate());
+        assertTrue(device.hasExpirationDate());
+        assertFalse(device.hasDonationIdNumber());
+        assertFalse(device.isLabeledContainsNRL());
+        assertFalse(device.isLabeledNoNRL());
+        assertTrue(device.requiresRX());
+        assertFalse(device.isOTC());
+    }
+
+    @Test
+    public void testCalendars() throws Exception {
+        Calendar publishDate = Calendar.getInstance();
+        publishDate.setTimeInMillis(1420696800000L);
+        assertNull(device.getCommercialDistributionEndDate());
+        assertEquals(publishDate, device.getPublishDate());
+    }
+
+    @Test
+    public void testIntegers() throws Exception {
+        assert(device.getDeviceCount() == 1);
+    }
+
+    @Test
+    public void testUDIDevice() throws Exception {
+        assertEquals("=/08717648200274=,000025=A99971312345600=>014032=}013032",
+                udiDevice.getUdi());
+        assertEquals("08717648200274", udiDevice.getDi());
+        assertEquals("ICCBBA", udiDevice.getIssuingAgency());
+        assertNull(udiDevice.getDonationId());
+        assertNull(udiDevice.getDonationId());
+        assertNull(udiDevice.getLotNumber());
+    }
+
 }
