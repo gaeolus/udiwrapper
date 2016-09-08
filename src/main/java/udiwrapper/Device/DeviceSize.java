cf641cf43bc2a836399a29192e35555cb25846a7
Ben Smith <bensmith4188@gmail.com>
2016-09-08 10:45:45 -0500

    implement device sizes

--- src/main/java/udiwrapper/Device/DeviceSize.java
+++ src/main/java/udiwrapper/Device/DeviceSize.java
@@ -1,10 +1,30 @@
 package udiwrapper.Device;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class DeviceSize {
 
-    public DeviceSize(JSONObject deviceSizeObject, String type){
-        // TODO find device sizes examples and implement device sizes.
+    private String type;
+    private HashMap<String, DeviceSizeInformation> sizeInformation = new HashMap<>();
+
+    public DeviceSize(JSONArray deviceSizeObject, String type) throws JSONException {
+        this.type = type;
+        for (int i = 0; i < deviceSizeObject.length(); i += 1){
+            JSONObject currentObject = deviceSizeObject.getJSONObject(i);
+            DeviceSizeInformation information = new DeviceSizeInformation(currentObject);
+            sizeInformation.put(information.getType(), information);
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public HashMap<String, DeviceSizeInformation> getSizeInformation() {
+        return sizeInformation;
     }
 }
---
+++ src/main/java/udiwrapper/Device/DeviceSizeInformation.java
@@ -0,0 +1,33 @@
+package udiwrapper.Device;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DeviceSizeInformation {
+    private String type;
+    private NameValuePair size;
+    private String sizeText;
+
+    public DeviceSizeInformation(JSONObject deviceSizeObject) throws JSONException {
+        if (!deviceSizeObject.isNull("sizeType")) type = deviceSizeObject.getString("sizeType");
+        if (!deviceSizeObject.isNull("size")){
+            JSONObject namevaluepair = deviceSizeObject.getJSONObject("size");
+            String unit = namevaluepair.getString("unit");
+            String value = namevaluepair.getString("value");
+            size = new NameValuePair(unit, value);
+        }
+        if (!deviceSizeObject.isNull("sizeText")) sizeText = deviceSizeObject.getString("sizeText");
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public NameValuePair getSize(){
+        return size;
+    }
+
+    public String getSizeText(){
+        return sizeText;
+    }
+}
