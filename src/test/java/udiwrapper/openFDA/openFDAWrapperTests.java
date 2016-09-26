f4580b14c3c3b72ab4528b59d859ee2d4edb5451
Ben Smith <bensmith4188@gmail.com>
2016-09-26 17:20:44 -0500

    test: add fda wrapper tests
    So far we're only testing for whether or not the device
    exists. Since the class doesn't even exist yet, it'll fail.
    
    Closes #3

---
+++ src/test/java/udiwrapper/openFDA/openFDAWrapperTests.java
@@ -0,0 +1,18 @@
+package udiwrapper.openFDA;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class openFDAWrapperTests {
+
+    @Test
+    public void testGoodDiExists(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testBadDiExists(){
+        assertFalse(true);
+    }
+}
