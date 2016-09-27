15da5c7cb83552c72bcbbd572cbad947ea4da20e
Ben Smith <bensmith4188@gmail.com>
2016-09-26 23:49:45 -0500

    WIP: test different features of the openFDA API
    Searching, counting, and skipping are the cool features
    of this API. These tests should be good. It'll also
    help to get a handle on how the UDIWrapper class should
    be implemented.
    
    See #5

--- src/test/java/udiwrapper/openFDA/openFDAWrapperTests.java
+++ src/test/java/udiwrapper/openFDA/openFDAWrapperTests.java
@@ -29,4 +29,26 @@ public class openFDAWrapperTests {
     public void testBadDiExists(){
         assertFalse(udiWrapper.getDeviceExists("something"));
     }
+
+    @Test
+    public void testDefaultSearch(){
+        assertNotNull(udiWrapper.fetchDevice("08717648200274"));
+    }
+
+    @Test
+    public void testSearching(){
+        int numberReturned = 262;
+        assertEquals(numberReturned, udiWrapper.setSearch("brand_name", "XIENCE").getTotal());
+    }
+
+    @Test
+    public void testSkipping(){
+        assertTrue(udiWrapper.setSkip("261").setSearch("brand_name", "XIENCE").getDeviceExists());
+        assertFalse(udiWrapper.setSkip("262").setSearch("brand_name", "XIENCE").getDeviceExists());
+    }
+
+    @Test
+    public void testCounting(){
+        assertTrue(udiWrapper.setCount("brand_name").getDeviceExists());
+    }
 }
